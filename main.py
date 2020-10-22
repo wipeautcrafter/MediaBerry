e5b5fdba000087945918cca25f8c94c21c8518b6
wipeautcrafter <wipeautcrafter@gmail.com>
2020-10-22 16:09:31 +0100

    a

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import unquote
+import subprocess
 import time
 import os
 
 hostname = 'localhost'
-port = 8080
+port = 80
 
-current = ''
+current = None
 
 commands = {
     'spotify': {
@@ -13,20 +15,20 @@ commands = {
         'stop': lambda: 'systemctl stop raspotify'
     },
     'radio': {
-        'start': lambda addr: f'mplayer {addr} &',
+        'start': lambda addr: f'mplayer {addr} -cache 1024 -cache-min 30 &',
         'stop': lambda: 'killall mplayer'
     }
 }
 
-def RunService(service, **args):
+def RunService(service, *args):
     global current
 
-    if current != '':
-        os.system(commands[service]['stop']())
+    if not current is None:
+        os.system(commands[current]['stop']())
 
     if service in commands:
-        os.system(commands[service]['start'](**args))
         current = service
+        os.system(commands[current]['start'](*args))
 
 class HttpServer(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -40,12 +42,25 @@ class HttpServer(BaseHTTPRequestHandler):
             self.wfile.write(open('index.html', 'r').read().encode('utf-8'))
 
         if self.path == '/status':
+            vol_out = subprocess.check_output('amixer sget Headphone', shell=True)
+            vol_line = str(vol_out.splitlines()[4])
+            volume = vol_line[vol_line.index('[')+1:vol_line.index('%')]
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            self.wfile.write(('{"volume":%s,"current":"%s"}' % (volume, current)).encode('utf-8'))
+
+        if self.path.startswith('/volume?'):
+            os.system("amixer sset Headphone " + self.path[8:] + "%")
+
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
 
         if self.path.startswith('/radio?'):
-            RunService('radio', self.path[7:])
+            RunService('radio', unquote(self.path[7:]))
 
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
@@ -61,7 +76,7 @@ class HttpServer(BaseHTTPRequestHandler):
 if __name__ == '__main__':
     RunService('spotify')
 
-    web_server = HTTPServer((hostname, port), HttpServer)
+    web_server = HTTPServer(('', port), HttpServer)
     print('Server started http://%s:%s' % (hostname, port))
 
     try:
